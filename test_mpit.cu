8773f852803f0526c5b61ef1d68439e26210c2ad
Matthias <mattwi@fysik.dtu.dk>
2018-02-13 23:41:29 +0100

    modified test_mpit.cu

--- test_mpit.cu
+++ test_mpit.cu
@@ -9,16 +9,13 @@ int main(int, char **);
 
 int main(int argc, char **argv)
 {
-   int i, l, dmmy, task, ntasks, thread, nthreads, cpuid;
+   int l, task, ntasks, thread, nthreads, cpuid;
    char node_name[MPI_MAX_PROCESSOR_NAME];
-
-
 
    // determine MPI task and total number of MPI tasks 
    MPI_Init(&argc, &argv);
    MPI_Comm_rank(MPI_COMM_WORLD, &task);
    MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
-
 
    // determine total number of OpenMP threads, thread number, 
    // associated core and hostname
@@ -27,18 +24,19 @@ int main(int argc, char **argv)
    {
        if(task==i)
        {
-   #pragma omp parallel for private(thread, cpuid)
-   for (i=0; i<nthreads; i++) {
-      thread = omp_get_thread_num(); 
-      cpuid = sched_getcpu();
-      dmmy = MPI_Get_processor_name(&node_name[0], &l);
-      printf("node %s, task %d of  %d tasks, thread %d of %d threads, on cpu  %d \n", 
-             &node_name[0], task, ntasks, thread, nthreads, cpuid);
-   }
+           #pragma omp parallel for private(thread, cpuid)
+           for (int k=0; k<nthreads; k++) {
+              thread = omp_get_thread_num(); 
+              cpuid = sched_getcpu();
+              MPI_Get_processor_name(&node_name[0], &l);
+              printf("node %s, task %d of  %d tasks, thread %d of %d threads, on cpu  %d \n", 
+                     &node_name[0], task, ntasks, thread, nthreads, cpuid);
+           }
        }
        MPI_Barrier(MPI_COMM_WORLD);
    }
 
+   MPI_Finalize();
    
    return 0;
 }
