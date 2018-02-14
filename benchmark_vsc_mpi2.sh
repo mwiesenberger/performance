14c689513d00d8619cd4179fcf18e303ac2a8e24
Matthias Wiesenberger <mattwi@fysik.dtu.dk>
2018-02-14 14:37:39 +0100

    added vsc3 cpu node benchmark file

--- benchmark_vsc_mpi2.sh
+++ benchmark_vsc_mpi2.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 #SBATCH -J benchmark
-#SBATCH -N 4 -n 8 --ntasks-per-socket=1 -c 24 --hint=memory_bound
-#SBATCH --partition=skl_fua_prod
-#SBATCH --account=FUA32_FELTOR
-#SBATCH --mem=182000
+#SBATCH -N 2 -n 4 --ntasks-per-socket=1 -c 8
 #SBATCH --time=12:00:00
 
 
 ###please specify a info file (for humans)
-#SBATCH -o "benchmark_skl_mpi4.info"
-FILENAME='benchmark_skl_mpi4' # name of output file
-NAME='Marconi Skylake'
-PROGRAM='cluster_skl'
+#SBATCH -o "benchmark_vsc_mpi2.info"
+FILENAME='benchmark_vsc_mpi2' # name of output file
+NAME='VSC3 cpus'
+PROGRAM='cluster_mpib'
 REPETITIONS=10
+COMMAND='srun --mpi=pmi2'
 
 
 echo "$NAME"
@@ -22,18 +20,17 @@ hostname
 date
 module list
 git rev-parse --verify HEAD
-make cluster_mpib device=skl
-#mv cluster_mpib $PROGRAM
+make cluster_mpib device=omp
+make ping_mpit device=omp
 
-srun --mpi=pmi2 ./ping_mpit
+$COMMAND ./ping_mpit
 
-COMMAND='srun --mpi=pmi2'
 FILE=""$FILENAME".csv"
 echo '"npx" "npy" "npz" "procs" "threads" "n" "Nx" "Ny" "Nz" "scal" "axpby" "pointwiseDot" "dot" "dx" "dy" "dz" "arakawa" "iterations" "cg" "ds" "exblas_d" "exblas_i"' > $FILE
 for ((n=0;n<$REPETITIONS;n++)); do
     for o in 2 3 4 5; do
         for N in 128 256 384 512 768 1024 1536 2048; do
-                echo "2 4 1 $o $N $N 1" | $COMMAND ./$PROGRAM >> $FILE
+                echo "2 2 1 $o $N $N 1" | $COMMAND ./$PROGRAM >> $FILE
         done
     done
 done
--- benchmark_vsc_mpi4.sh
+++ benchmark_vsc_mpi4.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 #SBATCH -J benchmark
-#SBATCH -N 4 -n 8 --ntasks-per-socket=1 -c 24 --hint=memory_bound
-#SBATCH --partition=skl_fua_prod
-#SBATCH --account=FUA32_FELTOR
-#SBATCH --mem=182000
+#SBATCH -N 4 -n 8 --ntasks-per-socket=1 -c 8
 #SBATCH --time=12:00:00
 
 
 ###please specify a info file (for humans)
-#SBATCH -o "benchmark_skl_mpi4.info"
-FILENAME='benchmark_skl_mpi4' # name of output file
-NAME='Marconi Skylake'
-PROGRAM='cluster_skl'
+#SBATCH -o "benchmark_vsc_mpi4.info"
+FILENAME='benchmark_vsc_mpi4' # name of output file
+NAME='VSC3 cpus'
+PROGRAM='cluster_mpib'
 REPETITIONS=10
+COMMAND='srun --mpi=pmi2'
 
 
 echo "$NAME"
@@ -22,12 +20,11 @@ hostname
 date
 module list
 git rev-parse --verify HEAD
-make cluster_mpib device=skl
-#mv cluster_mpib $PROGRAM
+make cluster_mpib device=omp
+make ping_mpit device=omp
 
-srun --mpi=pmi2 ./ping_mpit
+$COMMAND ./ping_mpit
 
-COMMAND='srun --mpi=pmi2'
 FILE=""$FILENAME".csv"
 echo '"npx" "npy" "npz" "procs" "threads" "n" "Nx" "Ny" "Nz" "scal" "axpby" "pointwiseDot" "dot" "dx" "dy" "dz" "arakawa" "iterations" "cg" "ds" "exblas_d" "exblas_i"' > $FILE
 for ((n=0;n<$REPETITIONS;n++)); do
