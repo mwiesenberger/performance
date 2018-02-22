8b13aab3775be2e1bcde6f864ba2d8858d7d7c2e
Matthias Wiesenberger <Matthias.Wiesenberger@uibk.ac.at>
2018-02-22 13:31:01 +0100

    updated knl benchmark results

--- submit_knl_mpi4.sh
+++ submit_knl_mpi4.sh
@@ -32,7 +32,7 @@ FILE=""$FILENAME".csv"
 echo '"npx" "npy" "npz" "procs" "threads" "n" "Nx" "Ny" "Nz" "scal" "axpby" "pointwiseDot" "dot" "dx" "dy" "dz" "arakawa" "iterations" "cg" "ds" "exblas_d" "exblas_i"' > $FILE
 for ((n=0;n<$REPETITIONS;n++)); do
     for o in 2 3 4 5; do
-        for N in 128 256 384 512 768 1024 1536 2048; do
+        for N in 136 272 408 544 816 1088 1632 2176; do
                 echo "2 2 1 $o $N $N 1" | $COMMAND ./$PROGRAM >> $FILE
         done
     done
