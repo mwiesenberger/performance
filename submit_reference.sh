8b8421f5af62e92d3fd1df721cc82d3f3d386a52
Matthias Wiesenberger <mattwi@fysik.dtu.dk>
2018-02-23 11:30:18 +0100

    added intel reference file and added new skl benchmark files, updated latency determination

---
+++ submit_reference.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#SBATCH -J benchmark
+#SBATCH -N 1 -n 2 --ntasks-per-socket=1 -c 24 --hint=memory_bound
+#SBATCH --partition=skl_fua_prod 
+#SBATCH --account=FUA32_FELTOR
+#SBATCH --mem=182000
+#SBATCH --time=12:00:00
+
+
+###please specify a info file (for humans)
+#SBATCH -o "benchmark_reference.info"
+FILENAME='benchmark_reference' # name of output file
+NAME='Marconi Skylake'
+PROGRAM='cluster_skl'
+REPETITIONS=1
+
+
+echo "$NAME"
+echo "$REPETITIONS"
+hostname 
+date
+module list
+git rev-parse --verify HEAD
+make cluster_mpib device=skl
+#mv cluster_mpib $PROGRAM
+
+srun --mpi=pmi2 ./ping_mpit
+
+COMMAND='srun --mpi=pmi2'
+FILE=""$FILENAME".csv"
+echo '"npx" "npy" "npz" "procs" "threads" "n" "Nx" "Ny" "Nz" "scal" "axpby" "pointwiseDot" "dot" "dx" "dy" "dz" "arakawa" "iterations" "cg" "ds" "exblas_d" "exblas_i"' > $FILE
+for ((n=0;n<$REPETITIONS;n++)); do
+    for o in 2 3 4 5; do
+        for N in 128 136 256 272 384 408 512 544 768 816 1024 1088 1536 1632 2048 2176; do
+            echo "1 2 1 $o $N $N 1" | $COMMAND ./$PROGRAM >> $FILE
+        done
+    done
+done
+date
