610a270fbc9dbba87bb7cb6aff685a13cd5bec91
Matthias Wiesenberger <Matthias.Wiesenberger@uibk.ac.at>
2018-02-23 21:23:19 +0100

    update knl data

--- submit_knl_mpi4.sh
+++ submit_knl_mpi4.sh
@@ -12,7 +12,6 @@
 #SBATCH -o "benchmark_knl_mpi4.info"
 FILENAME='benchmark_knl_mpi4' # name of output file
 NAME='Marconi KNL'
-PROGRAM='cluster_knl'
 REPETITIONS=10
 
 export OMP_NUM_THREADS=136
@@ -23,7 +22,6 @@ hostname
 module list
 git rev-parse --verify HEAD
 make cluster_mpib device=knl
-#mv cluster_mpib $PROGRAM
 
 srun --mpi=pmi2 ./ping_mpit
 
@@ -33,7 +31,7 @@ echo '"npx" "npy" "npz" "procs" "threads" "n" "Nx" "Ny" "Nz" "scal" "axpby" "poi
 for ((n=0;n<$REPETITIONS;n++)); do
     for o in 2 3 4 5; do
         for N in 136 272 408 544 816 1088 1632 2176; do
-                echo "2 2 1 $o $N $N 1" | $COMMAND ./$PROGRAM >> $FILE
+            echo "2 2 1 $o $N $N 1" | $COMMAND ./cluster_mpib >> $FILE
         done
     done
 done
