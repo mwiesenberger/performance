140c97059574413ab356d054b65bb9deac87ee3d
Matthias <mattwi@fysik.dtu.dk>
2018-10-17 14:00:36 +0200

    Add new submit files for 3d benchmarks
    The first ones are to be used on the applications
    branch on marconi.

---
+++ data3d/submit_knl_mpi8.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#SBATCH -J benchmark
+#SBATCH -N 8 -n 8 --ntasks-per-node=1 -c 272
+#SBATCH --partition=knl_fua_prod
+#SBATCH --constraint=cache
+#SBATCH --account=FUA22_FELTOR
+#SBATCH --time=12:00:00
+
+
+###please specify a info file (for humans)
+#SBATCH -o "benchmark_knl_mpi8.info"
+FILENAME='benchmark_knl_mpi8' # name of output file
+NAME='Marconi KNL'
+REPETITIONS=10
+
+export OMP_NUM_THREADS=136
+
+echo "$NAME"
+echo "$REPETITIONS"
+hostname
+date
+module list
+git rev-parse --verify HEAD
+make cluster_mpib device=knl
+
+srun --mpi=pmi2 ./ping_mpit
+
+COMMAND='srun --mpi=pmi2'
+FILE=""$FILENAME".csv"
+echo '"npx" "npy" "npz" "procs" "threads" "n" "Nx" "Ny" "Nz" "scal" "axpby" "pointwiseDot" "dot" "dx" "dy" "dz" "arakawa" "iterations" "cg" "ds" "exblas_d" "exblas_i"' > $FILE
+for ((n=0;n<$REPETITIONS;n++)); do
+    for o in 3 4; do
+        for N in 64 128 256 384 512; do
+            for Nz in 16 48 96 192 256; do
+            echo "1 1 8 $o $N $Nz 1" | $COMMAND >> $FILE
+        done
+    done
+done
+date
---
+++ data3d/submit_p100_mpi.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+#SBATCH -J benchmark
+#SBATCH -N 1 -n 8 --qos= XXX -p XXX
+#SBATCH --time=10:00:00
+
+###please specify a info file (for humans)
+#SBATCH -o "benchmark_v100nv_mpi.info"
+FILENAME='benchmark_v100nv_mpi' # name of output file
+NAME='Nvidia Tesla V100 with NVLink (DAVIDE)'
+REPETITIONS=10
+
+
+echo "$NAME"
+echo "$REPETITIONS"
+hostname
+date
+module list
+git rev-parse --verify HEAD
+make cluster_mpib device=gpu
+mpirun -n 1 ./ping_mpit
+mpirun -n 2 ./ping_mpit
+mpirun -n 4 ./ping_mpit
+mpirun -n 8 ./ping_mpit
+
+COMMAND='mpirun -n 1 ./cluster_mpib'
+FILE=""$FILENAME"1.csv"
+echo '"npx" "npy" "npz" "procs" "threads" "n" "Nx" "Ny" "Nz" "scal" "axpby" "pointwiseDot" "dot" "dx" "dy" "dz" "arakawa" "iterations" "cg" "ds" "exblas_d" "exblas_i"' > $FILE
+for ((n=0;n<$REPETITIONS;n++)); do
+    for o in 3 4; do
+        for N in 64 128 256 384 512; do
+            for Nz in 16 48 96 192 256; do
+            echo "1 1 1 $o $N $Nz 1" | $COMMAND >> $FILE
+        done
+    done
+done
+
+COMMAND='mpirun -n 2 ./cluster_mpib'
+FILE=""$FILENAME"2.csv"
+echo '"npx" "npy" "npz" "procs" "threads" "n" "Nx" "Ny" "Nz" "scal" "axpby" "pointwiseDot" "dot" "dx" "dy" "dz" "arakawa" "iterations" "cg" "ds" "exblas_d" "exblas_i"' > $FILE
+for ((n=0;n<$REPETITIONS;n++)); do
+    for o in 3 4; do
+        for N in 64 128 256 384 512; do
+            for Nz in 16 48 96 192 256; do
+            echo "1 1 2 $o $N $Nz 1" | $COMMAND >> $FILE
+        done
+    done
+done
+
+COMMAND='mpirun -n 4 ./cluster_mpib'
+FILE=""$FILENAME"4.csv"
+echo '"npx" "npy" "npz" "procs" "threads" "n" "Nx" "Ny" "Nz" "scal" "axpby" "pointwiseDot" "dot" "dx" "dy" "dz" "arakawa" "iterations" "cg" "ds" "exblas_d" "exblas_i"' > $FILE
+for ((n=0;n<$REPETITIONS;n++)); do
+    for o in 3 4; do
+        for N in 64 128 256 384 512; do
+            for Nz in 16 48 96 192 256; do
+            echo "1 1 4 $o $N $Nz 1" | $COMMAND >> $FILE
+        done
+    done
+done
+
+COMMAND='mpirun -n 8 ./cluster_mpib'
+FILE=""$FILENAME"8.csv"
+echo '"npx" "npy" "npz" "procs" "threads" "n" "Nx" "Ny" "Nz" "scal" "axpby" "pointwiseDot" "dot" "dx" "dy" "dz" "arakawa" "iterations" "cg" "ds" "exblas_d" "exblas_i"' > $FILE
+for ((n=0;n<$REPETITIONS;n++)); do
+    for o in 3 4; do
+        for N in 64 128 256 384 512; do
+            for Nz in 16 48 96 192 256; do
+            echo "1 1 8 $o $N $Nz 1" | $COMMAND >> $FILE
+        done
+    done
+done
+date
