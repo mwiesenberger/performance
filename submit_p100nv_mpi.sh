2f0e73b064a2fce2eb393da9ed0826105d76f989
Matthias <mattwi@fysik.dtu.dk>
2019-03-27 19:39:33 +0100

    Add 2d Davide benchmark

---
+++ submit_p100nv_mpi.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#SBATCH -J benchmark
+#SBATCH -N 1 -n 4 -A fusio_d_wiesen -p dvd_usr_prod
+#SBATCH --time=10:00:00
+
+###please specify a info file (for humans)
+#SBATCH -o "benchmark_p100nv_mpi.info"
+FILENAME='benchmark_p100nv_mpi' # name of output file
+NAME='Nvidia Tesla P100 with NvLink (DAVIDE)'
+REPETITIONS=10
+
+
+echo "$NAME"
+echo "$REPETITIONS"
+hostname 
+date
+module list
+git rev-parse --verify HEAD
+make cluster_mpib device=gpu
+mpirun -n 1 ./ping_mpit
+mpirun -n 2 ./ping_mpit
+mpirun -n 4 ./ping_mpit
+
+COMMAND='mpirun -n 1 ./cluster_mpib'
+FILE=""$FILENAME"1.csv"
+echo '"npx" "npy" "npz" "procs" "threads" "n" "Nx" "Ny" "Nz" "scal" "axpby" "pointwiseDot" "dot" "dx" "dy" "dz" "arakawa" "iterations" "cg" "ds" "exblas_d" "exblas_i"' > $FILE
+for ((n=0;n<$REPETITIONS;n++)); do
+    for o in 2 3 4 5; do
+        for N in 128 256 384 512 768 1024 1536 2048; do
+            echo "1 1 1 $o $N $N 1" | $COMMAND >> $FILE
+        done
+    done
+done
+
+COMMAND='mpirun -n 2 ./cluster_mpib'
+FILE=""$FILENAME"2.csv"
+echo '"npx" "npy" "npz" "procs" "threads" "n" "Nx" "Ny" "Nz" "scal" "axpby" "pointwiseDot" "dot" "dx" "dy" "dz" "arakawa" "iterations" "cg" "ds" "exblas_d" "exblas_i"' > $FILE
+for ((n=0;n<$REPETITIONS;n++)); do
+    for o in 2 3 4 5; do
+        for N in 128 256 384 512 768 1024 1536 2048; do
+            echo "1 2 1 $o $N $N 1" | $COMMAND >> $FILE
+        done
+    done
+done
+
+COMMAND='mpirun -n 4 ./cluster_mpib'
+FILE=""$FILENAME"4.csv"
+echo '"npx" "npy" "npz" "procs" "threads" "n" "Nx" "Ny" "Nz" "scal" "axpby" "pointwiseDot" "dot" "dx" "dy" "dz" "arakawa" "iterations" "cg" "ds" "exblas_d" "exblas_i"' > $FILE
+for ((n=0;n<$REPETITIONS;n++)); do
+    for o in 2 3 4 5; do
+        for N in 128 256 384 512 768 1024 1536 2048; do
+            echo "2 2 1 $o $N $N 1" | $COMMAND >> $FILE
+        done
+    done
+done
+date
